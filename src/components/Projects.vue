adfe9807aa60ff8c8086cfba5867a10e97b0defb
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-01-30 17:16:06 -0600

    Mobile - Add Project, ProjectCard and Pill components and refactor all components to SEO friendly

---
+++ src/components/Projects.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import ProjectCard from './shared/ProjectCard.vue';
+
+const projects = [
+  {
+    title: "Build a E-Commerce App",
+    description: "Web app for managing eCommerce operations: track sales, manage inventory in real-time, process payments securely, and track orders. Gain insights into product performance, create personalized promotions, and manage discounts to boost engagement and drive sales.",
+    src: "/src/assets/poktapok.png",
+    techStack: ['Shopify', 'React', 'Tailwind CSS']
+  },
+  {
+    title: "Build a E-Commerce App",
+    description: "Web app for managing eCommerce operations: track sales, manage inventory in real-time, process payments securely, and track orders. Gain insights into product performance, create personalized promotions, and manage discounts to boost engagement and drive sales.",
+    src: "/src/assets/poktapok.png",
+    techStack: ['Shopify', 'React', 'Tailwind CSS']
+  },
+  {
+    title: "Build a E-Commerce App",
+    description: "Web app for managing eCommerce operations: track sales, manage inventory in real-time, process payments securely, and track orders. Gain insights into product performance, create personalized promotions, and manage discounts to boost engagement and drive sales.",
+    src: "/src/assets/poktapok.png",
+    techStack: ['Shopify', 'React', 'Tailwind CSS']
+  }
+];
+</script>
+
+<template>
+  <section class="projects" aria-labelledby="projects-heading">
+    <h2 id="projects-heading" class="projects-title">PROJECTS</h2>
+    <div class="projects-grid" role="list">
+      <ProjectCard
+        v-for="project in projects"
+        :key="project.title"
+        :title="project.title"
+        :description="project.description"
+        :src="project.src"
+        :tech-stack="project.techStack"
+        role="listitem"
+      />
+    </div>
+  </section>
+</template>
---
+++ src/components/shared/ProjectCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Pill from '../shared/Pill.vue';
+defineProps<{
+  title: string;
+  description: string;
+  src: string;
+  techStack: string[];
+}>();
+</script>
+
+<template>
+  <article class="project-card" role="listitem">
+    <h3 class="project-card-title">{{ title }}</h3>
+    <figure class="project-card-image">
+      <img :src="src" :alt="`${title} project preview`" />
+    </figure>
+    <article class="project-card-content">
+      <p>{{ description }}</p>
+    </article>
+    <footer class="project-card-tech-stack" role="list">
+      <Pill 
+        v-for="tech in techStack" 
+        :key="tech" 
+        :title="tech"
+        role="listitem"
+      />
+    </footer>
+  </article>
+</template>
