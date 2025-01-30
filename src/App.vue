bede0f51e385d066f9a9dce279fe2f025db15b44
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-01-29 20:30:30 -0600

    Mobile - Add About component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import CoverLetter from './components/CoverLetter.vue'
+import CoverLetter from './components/CoverLetter.vue';
+import About from './components/About.vue';
 </script>
 
 <template>
   <div>
     <CoverLetter />
+    <About />
   </div>
 </template>
 
---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="about">
+    <h2 class="about-title">ABOUT</h2>
+    <div class="about-content">
+      <p>
+        I'm a software engineer passionate about developing scalable and high-performance solutions. I blend thoughtful design with robust engineering to create seamless user experiences that are optimized for usability and performance.
+      </p>
+      <p>
+        I specialize in front-end and back-end development, crafting user-friendly interfaces and scalable architectures. My expertise includes integrating cloud technologies like AWS and Docker to meet diverse business needs efficiently.
+      </p>
+      <p>
+        With experience in startups, digital agencies, and large enterprises, I've built complex applications, improved system performance, and ensured quality using tools like Cypress and Jest. My mission is to create future-proof solutions that deliver value to businesses and users alike.
+      </p>
+    </div>
+  </section>
+</template>
