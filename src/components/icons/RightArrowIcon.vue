b8406a86e4120116c16778dd505bfb81f549743f
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-01-30 18:00:15 -0600

    Mobile - Add View All The Projects link

---
+++ src/components/icons/RightArrowIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg 
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    class="arrow-icon"
+  >
+    <path 
+      d="M2 12H14M13.1714 12.0007L8.22168 7.05093L9.63589 5.63672L16 12.0007L9.63589 18.3646L8.22168 16.9504L13.1714 12.0007Z" 
+      stroke="currentColor"
+      fill="none"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
