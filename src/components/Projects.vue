f22516c874bd41265b3a513dfdcb7df992fef1a7
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-02-24 12:08:53 -0600

    Tablet - Create interfaces for Articles and Projects sections

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,15 +4,23 @@ import ProjectCard from './shared/ProjectCard.vue';
 import RightArrowIcon from './shared/icons/RightArrowIcon.vue';
 import { projects } from '../mocks/projects';
 
+interface Project {
+  title: string;
+  description: string;
+  src: string;
+  techStack: string[];
+}
+
+const canShowProjects = computed(() => projects.length >= 1);
 const showViewAll = computed(() => projects.length > 3);
 </script>
 
 <template>
-  <section class="projects" aria-labelledby="projects-heading">
+  <section v-if="canShowProjects" class="projects" aria-labelledby="projects-heading">
     <h2 id="projects-heading" class="projects-title">PROJECTS</h2>
     <div class="projects-grid" role="list">
       <ProjectCard
-        v-for="project in projects"
+        v-for="project in projects as Project[]"
         :key="project.title"
         :title="project.title"
         :description="project.description"
