1b902eb89509c4585518061fe059b48e75893ce2
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-02-24 10:54:16 -0600

    Tablet - Refactor the ProjectCard component to prepare the layout Desktop design and make semantic SEO-friendly

--- src/components/shared/ProjectCard.vue
+++ src/components/shared/ProjectCard.vue
@@ -11,19 +11,35 @@ defineProps<{
 <template>
   <article class="project-card" role="listitem">
     <h3 class="project-card-title">{{ title }}</h3>
-    <figure class="project-card-image">
-      <img :src="src" :alt="`${title} project preview`" />
-    </figure>
-    <article class="project-card-content">
-      <p>{{ description }}</p>
-    </article>
-    <footer class="project-card-tech-stack" role="list">
-      <Pill 
-        v-for="tech in techStack" 
-        :key="tech" 
-        :title="tech"
-        role="listitem"
-      />
-    </footer>
+    <div class="project-card-wrapper">
+      <figure class="project-card-image">
+        <img :src="src" :alt="`${title} project preview`" />
+      </figure>
+      <div class="project-card-info">
+        <article 
+          class="project-card-content" 
+          aria-label="Project description"
+          itemscope 
+          itemtype="http://schema.org/Article"
+        >
+          <p itemprop="description">{{ description }}</p>
+        </article>
+        <footer 
+          class="project-card-tech-stack" 
+          role="list" 
+          aria-label="Technologies used"
+          itemscope 
+          itemtype="http://schema.org/ItemList"
+        >
+          <Pill 
+            v-for="tech in techStack" 
+            :key="tech" 
+            :title="tech"
+            role="listitem"
+            itemprop="itemListElement"
+          />
+        </footer>
+      </div>
+    </div>
   </article>
 </template>
