140789541636b24fee45eaaf781fba70fed9aea7
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-03-25 00:13:04 -0600

    Main - Quick fix of the -size variables and change VNORIEGA to VICENTE NORIEGA in the CoverLetter component, I think looks much better

--- src/components/CoverLetter.vue
+++ src/components/CoverLetter.vue
@@ -8,7 +8,7 @@ import SocialMedia from './shared/SocialMedia.vue';
       <img src="/assets/profile-x4.png" alt="Vicente Noriega's profile photo">
     </figure>
     <article class="cover-letter">
-      <h1 class="title">VNORIEGA</h1>
+      <h1 class="title">VICENTE NORIEGA</h1>
       <h2 class="heading">Software Engineer</h2>
       <p class="body">I design and build digital experiences</p>
     </article>
