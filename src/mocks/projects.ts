2f0b2979d59f6b3c9de34295fb78a98915fde566
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-03-28 17:59:11 -0600

    Develop - Fix the scrollbar and the layouts of desktop, tablet and mobile

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -10,8 +10,8 @@ export const projects = [
     description: "Web app for managing eCommerce operations: track sales, manage inventory in real-time, process payments securely, and track orders. Gain insights into product performance, create personalized promotions, and manage discounts to boost engagement and drive sales.",
     src: "/assets/mid-ticket-checker.png",
     techStack: ['React Native', 'TypeScript', 'Tailwind CSS', 'MongoDB', 'DeepSeek', 'Lambda AWS']
-  },*/
-  /*{
+  },
+  {
     title: "Build an Appointment Agent",
     description: "Designed a web application with a two-column layout featuring an AI-powered chatbot for scheduling appointments and a compact calendar displaying available dates. Focused on user-friendly design, efficiency, and seamless doctor-patient interaction.",
     src: "/assets/mid-appointment-agent.png",
