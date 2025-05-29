cd75523e3a405d83994fcf8b81fab44d5d774160
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-05-29 17:07:16 -0600

    SEO - Optimize images weight

--- src/components/CoverLetter.vue
+++ src/components/CoverLetter.vue
@@ -5,7 +5,7 @@
 <template>
   <section class="cover-letter-section">
     <figure class="avatar">
-      <img src="/assets/profile-x4.png" alt="Vicente Noriega's profile photo" />
+      <img src="/assets/profile-x4.avif" alt="Vicente Noriega's profile photo" />
     </figure>
     <article class="cover-letter">
       <h1 class="title">VICENTE NORIEGA</h1>
