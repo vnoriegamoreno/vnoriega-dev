7e524f27f418ae60aac441e090610bd385279944
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-08-21 22:27:46 -0600

    [Feature]: feat/experience - Extract the job details into a component and pass the data as props.

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,5 +1,74 @@
 <script setup lang="ts">
-import Pill from './shared/Pill.vue';
+import { onMounted, ref } from 'vue';
+import JobDescription from './shared/JobDescription.vue';
+
+const experience = ref<any>([]);
+
+onMounted(() => {
+  experience.value = [
+    {
+      company: "Q2",
+      startDate: "2022",
+      endDate: "2024",
+      jobRole: "Staff Software Engineer",
+      position: "Remote",
+      resume: "At Q2, I architected and maintained two flagship banking apps with Vue.js and Ember.js, emphasizing modularity, performance, and responsive design. I refactored core components to accelerate feature delivery and introduced a CSS-variable theming system for UI consistency. Resolving TypeScript issues and deprecations improved stability, while expanding unit and E2E tests cut regressions and elevated quality.",
+      techStack: [
+        "CSS",
+        "Cypress.js",
+        "Ember.js",
+        "ESLint",
+        "Git",
+        "JavaScript",
+        "Jest",
+        "Mocha.js",
+        "TypeScript"
+      ],
+    },
+    {
+      company: "Unosquare",
+      startDate: "2020",
+      endDate: "2022",
+      jobRole: "Full Stack Developer",
+      position: "Remote",
+      resume: "As part of the FMI project, which offers advanced medical reporting for pathology patients, I was critical in enhancing the system's ability to process and validate complex reports. My work ensured the seamless integration of multiple services and databases, contributing to better medical documentation and patient outcomes. Additionally, I supported the QA automation team using the Cypress framework to ensure a high- quality product, while providing technical support to guarantee system functionality and reliability",
+      techStack: [
+        "AWS Lambda",
+        "Confluence",
+        "Cypress.js",
+        "Enzyme",
+        "Express",
+        "Jenkins",
+        "Jest",
+        "Lucid Chart",
+        "Oracle DB",
+        "React Testing Library",
+        "React",
+        "SonarQube",
+        "XML"
+      ],
+    },
+    {
+      company: "People Thrust",
+      startDate: "2019",
+      endDate: "2020",
+      jobRole: "Full Stack Developer",
+      position: "Remote",
+      resume: "At People Thrust, I contributed to two major projects: the development of a localization feature for Pluto TV and a full-stack real estate application. In both projects, I played a key role in creating scalable, responsive solutions that met user demands, while also focusing on optimizing both the front and back-end systems for performance and reliability.",
+      techStack: [
+        "Axios",
+        "Docker",
+        "Jenkins",
+        "Material-UI",
+        "NestJS",
+        "PostgreSQL",
+        "React",
+        "Redux",
+        "Swagger"
+      ],
+    },
+  ];
+});
 </script>
 
 <template>
@@ -7,44 +76,18 @@ import Pill from './shared/Pill.vue';
     <h2 id="experience-heading" class="experience-title">EXPERIENCE</h2>
   </section>
   <div class="experience-grid" role="list">
-    <article class="experience-details">
-      <h3 class="experience-details-job-period">
-        <span class="experience-details-job-period-start-year">
-          2022
-        </span>
-        <span class="experience-details-job-period-separator" />
-        <span class="experience-details-job-period-end-year">
-          2024
-        </span>
-      </h3>
-      <div class="experience-details-job-overview">
-        <h3 class="experience-details-job-overview-title">
-          Staff Software Engineer
-        </h3>
-        <p class="experience-details-job-overview-description">
-          At Q2, I architected and maintained two flagship banking apps with Vue.js and Ember.js, emphasizing modularity, performance, and responsive design. I refactored core components to accelerate feature delivery and introduced a CSS-variable theming system for UI consistency. Resolving TypeScript issues and deprecations improved stability, while expanding unit and E2E tests cut regressions and elevated quality.
-        </p>
-        <footer
-          class="project-card-tech-stack"
-          role="list"
-          aria-label="Technologies used"
-          itemscope
-          itemtype="http://schema.org/ItemList"
-        >
-          <Pill title="CSS" role="listitem" />
-          <Pill title="Ember.js" role="listitem" />
-          <Pill title="ESLint" role="listitem" />
-          <Pill title="Git" role="listitem" />
-          <Pill title="Git" role="listitem" />
-          <Pill title="JavaScript" role="listitem" />
-          <Pill title="Jest" role="listitem" />
-          <Pill title="Mocha.js" role="listitem" />
-          <Pill title="TypeScript" role="listitem" />
-          <Pill title="Vue Test Utils" role="listitem" />
-          <Pill title="Vue.js" role="listitem" />
-        </footer>
-      </div>
-    </article>
+    <JobDescription
+      v-for="jobDetail in experience as any[]"
+      :key="jobDetail.company"
+      :company="jobDetail.company"
+      :endDate="jobDetail.endDate"
+      :job-role="jobDetail.jobRole"
+      :position="jobDetail.position"
+      :resume="jobDetail.resume"
+      :startDate="jobDetail.startDate"
+      :tech-stack="jobDetail.techStack"
+      role="listitem"
+    />
   </div>
   <p class="experience-link">
     <a
---
+++ src/components/shared/JobDescription.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+  import Pill from '../shared/Pill.vue';
+  defineProps<{
+    company: string;
+    endDate: string;
+    jobRole: string;
+    position: string;
+    resume: string;
+    startDate: string;
+    techStack: string[];
+  }>();
+</script>
+
+<template>
+  <article class="experience-details">
+    <h3 class="experience-details-job-period">
+      <span class="experience-details-job-period-start-year">
+        {{ startDate }}
+      </span>
+      <span class="experience-details-job-period-separator" />
+      <span class="experience-details-job-period-end-year">
+        {{ endDate }}
+      </span>
+    </h3>
+    <div class="experience-details-job-overview">
+      <h3 class="experience-details-job-overview-title">
+        {{ company }}
+        <span class="experience-details-job-overview-title-separator">|</span>
+        {{ jobRole }}
+      </h3>
+      <p class="experience-details-job-overview-description">
+        {{ resume }}
+      </p>
+      <footer
+        class="project-card-tech-stack"
+        role="list"
+        aria-label="Technologies used"
+        itemscope
+        itemtype="http://schema.org/ItemList"
+      >
+        <Pill
+          v-for="tech in techStack"
+          :key="tech"
+          :title="tech"
+          role="listitem"
+          itemprop="itemListElement"
+        />
+      </footer>
+    </div>
+  </article>
+</template>
