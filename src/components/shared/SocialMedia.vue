ebde143c78470cb4ecceebc3b3b82db721a5c1bf
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2025-11-07 14:03:07 -0600

    [Refactor] - Add my HackerRank Profile to my Social Media shortcuts links

--- src/components/shared/SocialMedia.vue
+++ src/components/shared/SocialMedia.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
   import LinkedinIcon from './icons/LinkedinIcon.vue';
   import GithubIcon from './icons/GithubIcon.vue';
+import HackerRankIcon from './icons/HackerRankIcon.vue';
   // import CvIcon from './icons/CvIcon.vue';
 </script>
 
 <template>
   <nav class="social-media" aria-label="Social media links">
-    <!-- <a
-      href="/assets/resume.pdf"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Download CV"
-      download="resume.pdf"
-    >
-      <CvIcon />
-    </a> -->
     <a
       href="https://linkedin.com/in/vnoriegadev"
       target="_blank"
@@ -30,6 +22,14 @@
       aria-label="GitHub Profile"
     >
       <GithubIcon />
+    </a>
+    <a
+      href="https://www.hackerrank.com/profile/vnoriega"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="HackerRank Profile"
+    >
+      <HackerRankIcon />
     </a>
   </nav>
 </template>
